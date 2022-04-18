e85264e1cd6bb0bf320797e16892a0ada2da3b13
stran23 <98300336+stran23@users.noreply.github.com>
2022-04-18 16:26:01 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -14,15 +14,20 @@
  */
 function createPhotoMap () {
   // URL of a Google Sheets spreadsheet output as CSV
-  var csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSuESp280YE8_PokNYJSK_V3zxGh7ZJlDqMNeliCoz1kt6q2ii7lRovZ-yMlEFY2n6rhUu3vOpnvFGx/pub?gid=0&single=true&output=csv';
+  var csvUrl = '# GEOG 4046 Examples
+Example maps for assignments in GEOG 4046.
+
+## Current Weather
+A map showing real-time weather radar and alerts from the National Weather Service.
+<https://docs.google.com/spreadsheets/d/e/2PACX-1vRuhgaSOD8J9oRZOUH7MSNDBHasHbHh3otZDD9nm0ChqoOwExF2jehQFIaDMEbN5zfQCTCH94GNDr7T/pub?gid=0&single=true&output=csv';
   
   // create map object with center lat/lon and zoom level
   var map = L.map('map').setView([30.44, -91.187], 13);
   
   // create basemap object. See examples at https://leaflet-extras.github.io/leaflet-providers/preview/
-  var basemap = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
-  	maxZoom: 16,
-  	attribution: 'USGS'
+ var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
   
   // use Papa Parse (papaparse.com) to get the Google Sheets CSV
